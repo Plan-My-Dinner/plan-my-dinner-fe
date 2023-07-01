9334ed0abe74e42941a9935d84ecbd686accf078
Nathan Hodnett <nathanhodnett@gmail.com>
2023-07-01 14:47:28 -0600

    Add initial fetch call

--- src/Components/Homepage/Homepage.tsx
+++ src/Components/Homepage/Homepage.tsx
@@ -1,1 +1,25 @@
-export {}
+import React, { useEffect, useState } from 'react';
+import { fetchSingleRandomRecipe } from "../../apiCalls";
+
+const Homepage = () => {
+
+useEffect(() => {
+    const fetchData = async() => {
+        try {
+            const data = await fetchSingleRandomRecipe()
+            console.log('10', data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    fetchData()
+}, [])
+
+    return (
+        <div>
+            HI!
+        </div>
+    )
+}
+
+export default Homepage;
