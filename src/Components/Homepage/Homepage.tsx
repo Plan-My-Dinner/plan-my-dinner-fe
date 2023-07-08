88e2babe3f5d958d8fb13abaa3a89bb4eb7df969
Danielle Sweeny <dsweeny1@gmail.com>
2023-07-08 15:12:37 -0600

    Update random meal functionality

--- src/Components/Homepage/Homepage.tsx
+++ src/Components/Homepage/Homepage.tsx
@@ -14,23 +14,46 @@ const Homepage = () => {
     const [numberOfMeals, setNumberOfMeals] = useState<number>(5)
     const [randomMeals, setRandomMeals] = useState<RandomMealProps[]>([])
 
+// useEffect(() => {
+//     for (let i = 0; i < numberOfMeals; i++) {
+//         console.log('19', numberOfMeals)
+//         const fetchData = async() => {
+//             try {
+//                 const data = await fetchSingleRandomRecipe()
+//                 console.log('23', data.meals)
+//                 console.log('type', typeof data )
+//                 randomMeals.push(data.meals)
+//                 console.log('26', randomMeals)
+//             } catch (error) {
+//                 console.log(error)
+//             }
+//         }
+//         fetchData()
+//     }
+// }, [numberOfMeals])
+
 useEffect(() => {
-    for (let i = 0; i < numberOfMeals; i++) {
-        console.log('19', numberOfMeals)
-        const fetchData = async() => {
-            try {
-                const data = await fetchSingleRandomRecipe()
-                console.log('23', data.meals)
-                console.log('type', typeof data.meals )
-                randomMeals.push(data.meals)
-                console.log('26', randomMeals)
-            } catch (error) {
-                console.log(error)
-            }
+    const fetchData = async () => {
+      try {
+        const newMeals = [];
+        for (let i = 0; i < numberOfMeals; i++) {
+          const data = await fetchSingleRandomRecipe();
+          newMeals.push(...data.meals);
         }
-        fetchData()
-    }
-}, [numberOfMeals])
+  
+        const uniqueMeals = newMeals.filter(
+          (meal, index, self) => index === self.findIndex((m) => m.idMeal === meal.idMeal)
+        );
+  
+        setRandomMeals(uniqueMeals);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
+    setRandomMeals([]);
+    fetchData();
+  }, [numberOfMeals]);
 
     return (
         <div>
