7c0ca4e7bcb21772680a1ffceb98b1025fb2f941
Danielle Sweeny <dsweeny1@gmail.com>
2023-07-15 14:43:31 -0600

    Update Recipe card

--- src/Components/RecipeCard/RecipeCard.tsx
+++ src/Components/RecipeCard/RecipeCard.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { RandomMealProps } from "../Homepage/Homepage";
 import './RecipeCard.css';
+import lockedIcon from '../../Assets/lockedIcon.png'
+import unlockedIcon from '../../Assets/unlockedIcon.png'
 
 interface RecipeCardProps extends RandomMealProps {
     locked: boolean;
@@ -8,10 +10,22 @@ interface RecipeCardProps extends RandomMealProps {
 }
 
 const RecipeCard: React.FC<RecipeCardProps> = ({ strMeal, idMeal, strMealThumb, locked, setLocked }) => {
+
+    const handleLockClick = () => {
+        setLocked(!locked);
+    };
+
     return (
         <div className='recipe-container'>
-            <div className= 'recipe-card' id={idMeal} key={idMeal}>
-                <img className='recipe-image' src={strMealThumb} alt={strMeal}/>
+            <div className='recipe-card' id={idMeal} key={idMeal}>
+                <button onClick={handleLockClick}>
+                    {locked ? (
+                        <img src={unlockedIcon} alt='unlocked' />
+                    ) : (
+                        <img src={lockedIcon} alt='locked' />
+                    )}
+                </button>
+                <img className='recipe-image' src={strMealThumb} alt={strMeal} />
                 <h3 className='recipe-name'>{strMeal}</h3>
             </div>
         </div>
