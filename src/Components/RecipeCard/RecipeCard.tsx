79bc5eb79261ed9def4c6337d70f7b559f8182a4
Danielle Sweeny <dsweeny1@gmail.com>
2023-07-15 14:42:56 -0600

    fix merge

--- src/Components/RecipeCard/RecipeCard.tsx
+++ src/Components/RecipeCard/RecipeCard.tsx
@@ -1,17 +1,35 @@
-import React from "react"
-import { RandomMealProps } from "../Homepage/Homepage"
-import './RecipeCard.css'
+import React from "react";
+import { RandomMealProps } from "../Homepage/Homepage";
+import './RecipeCard.css';
+import lockedIcon from '../../Assets/lockedIcon.png'
+import unlockedIcon from '../../Assets/unlockedIcon.png'
 
-const RecipeCard: React.FC<RandomMealProps> = ({ strMeal, idMeal, strMealThumb, locked, setLocked }) => {
+interface RecipeCardProps extends RandomMealProps {
+    locked: boolean;
+    setLocked: (locked: boolean) => void;
+}
+
+const RecipeCard: React.FC<RecipeCardProps> = ({ strMeal, idMeal, strMealThumb, locked, setLocked }) => {
+
+    const handleLockClick = () => {
+        setLocked(!locked);
+    };
 
     return (
         <div className='recipe-container'>
-            <div className= 'recipe-card' id={idMeal} key={idMeal}>
-                <img className='recipe-image' src={strMealThumb} alt={strMeal}/>
+            <div className='recipe-card' id={idMeal} key={idMeal}>
+                <button onClick={handleLockClick}>
+                    {locked ? (
+                        <img src={unlockedIcon} alt='unlocked' />
+                    ) : (
+                        <img src={lockedIcon} alt='locked' />
+                    )}
+                </button>
+                <img className='recipe-image' src={strMealThumb} alt={strMeal} />
                 <h3 className='recipe-name'>{strMeal}</h3>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default RecipeCard;
