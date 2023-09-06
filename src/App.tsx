0f073d1bb829e17bb94b13259889bdc1d87b88dd
Nathan Hodnett <nathanhodnett@gmail.com>
2023-09-05 18:44:05 -0600

    Add dragable functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { DndContext } from '@dnd-kit/core';
 import Homepage from './Components/Homepage/Homepage';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <Homepage />
+      <DndContext>
+        <Homepage />
+      </DndContext>
     </div>
   );
 }
--- src/Components/RecipeCard/RecipeCard.tsx
+++ src/Components/RecipeCard/RecipeCard.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { RandomMealProps } from '../../types';
 import './RecipeCard.css';
+import {useDraggable} from '@dnd-kit/core';
+import {CSS} from '@dnd-kit/utilities';
 import lockedIcon from '../../Assets/lockedIcon.png';
 import unlockedIcon from '../../Assets/unlockedIcon.png';
 import emptyHeart from '../../Assets/empty-heart.png'
@@ -10,17 +12,27 @@ interface RecipeCardProps extends RandomMealProps {
 }
 
 const RecipeCard: React.FC<RecipeCardProps> = ({ strMeal, idMeal, strMealThumb, locked, toggleLock }) => {
+  
+  const {attributes, listeners, setNodeRef, transform} = useDraggable({
+    id: idMeal
+  });
+  const style = {
+    transform: CSS.Translate.toString(transform),
+  };
+  
   return (
     <div className="recipe-container">
       <div className="recipe-card" id={idMeal}>
         <button className='lock-button' onClick={() => toggleLock(idMeal)}>
           {locked ? <img src={lockedIcon} alt="locked" /> : <img src={unlockedIcon} alt="unlocked" />}
         </button>
+        <button ref={setNodeRef} style={style} {...listeners} {...attributes}>
         <img className="recipe-image" src={strMealThumb} alt={strMeal} />
         <button className='favorite-button'>
           <img src={emptyHeart} className='empty-heart' alt='empty-heart'></img>
         </button>
         <h3 className="recipe-name">{strMeal}</h3>
+        </button>
       </div>
     </div>
   );
